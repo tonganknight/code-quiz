69594016f4d00fd139c509142810619a44cc70f1
Malachi <malachialusa@gmail.com>
2020-11-20 22:53:40 -0700

    Added all qestions, and logic.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,20 @@
 document.getElementById("startbutton").addEventListener("click",function test(){
     
+    function wrong(){
+        clearInterval(startCountdown);
+        window.alert("Wrong! -10secs, try again");
+
+        time= time-9
+
+        timer()
+
+        startCountdown=setInterval(timer,1000);
+        }
+
+    function gameover()
+
+
+
     //My timer function
     var time =60
     var timer =function(){
@@ -8,8 +23,9 @@ document.getElementById("startbutton").addEventListener("click",function test(){
             time--;
 
             
-             //if (time === 0){// game over rule
-                // } 
+             if (time === 0){
+                 Gameover()
+              } 
     }
 
     var startCountdown =setInterval(timer,1000);// starts the countdown  
@@ -23,16 +39,7 @@ document.getElementById("startbutton").addEventListener("click",function test(){
     var button3 = document.getElementById("button3")
     var button4 = document.getElementById("button4")
      
-    function wrong(){
-        clearInterval(startCountdown);
-        window.alert("Wrong! -10secs, try again");
-
-        time= time-9
-
-        timer()
-
-        startCountdown=setInterval(timer,1000);
-    }
+    
 
     //creating buttons             
     function createbuttons(){
@@ -103,18 +110,247 @@ document.getElementById("startbutton").addEventListener("click",function test(){
 
     createbuttons()// create question buttons
 
-    //question2
 
-    function question2(){
+    //question7
 
-        var question = document.getElementById("question")
-        var q2 =question.innerHTML ='The push() function does what?'
+    function question7(){
+
+        clearbuttons()
+
+        var question = document.getElementById("question");
+        var q2 =question.innerHTML ="For Variable X, How would I show an increment to X ?"
+
+        document.getElementById("button1").textContent = "X--";
+        document.getElementById("button2").textContent = "X++";
+        document.getElementById("button3").textContent = "X=X+X";
+        document.getElementById("button4").textContent = "X=+X"
+
+        document.getElementById("button1").addEventListener("click", function(){
+            wrong()
+         }); 
+ 
+        document.getElementById("button2").addEventListener("click", function(){
+            gameover()
+        });
+
+        document.getElementById("button3").addEventListener("click", function(){
+            wrong()
+        });
+        
+        document.getElementById("button4").addEventListener("click", function(){
+            wrong()
+        });
+
+        
+        
+    }
+
+    //question6
+
+    function question6(){
+
+        clearbuttons()
+
+        var question = document.getElementById("question");
+        var q2 =question.innerHTML ="What does the function 'slice()' do?"
+
+        document.getElementById("button1").textContent = "Removes a selected position in an array";
+        document.getElementById("button2").textContent = "Cuts out a function from a sheet";
+        document.getElementById("button3").textContent = "Stops Event Bubbling";
+        document.getElementById("button4").textContent = "Removes a variable";
+
+        document.getElementById("button1").addEventListener("click", function(){
+            question7()
+         }); 
+ 
+        document.getElementById("button2").addEventListener("click", function(){
+            wrong()
+        });
+
+        document.getElementById("button3").addEventListener("click", function(){
+            question6()
+        });
+        
+        document.getElementById("button4").addEventListener("click", function(){
+            wrong()
+        });
 
     }
 
 
+    //question5
+    function question5(){
+
+        clearbuttons()
+
+        var question = document.getElementById("question");
+        var q2 =question.innerHTML ='In the bubbling, what is the deepest nested event referred as?'
+
+        document.getElementById("button1").textContent = "Event";
+        document.getElementById("button2").textContent = "The DOM";
+        document.getElementById("button3").textContent = "Target";
+        document.getElementById("button4").textContent = "Event Listener ";
+
+        document.getElementById("button1").addEventListener("click", function(){
+            wrong()
+         }); 
+ 
+ 
+         document.getElementById("button2").addEventListener("click", function(){
+             wrong()
+         });
+ 
+         document.getElementById("button3").addEventListener("click", function(){
+             question6()
+         });
+         
+         document.getElementById("button4").addEventListener("click", function(){
+             wrong()
+         });
+
+    }
+
+
+
+    //question4
+
+    function question4(){
+        
+        clearbuttons()
+
+        var question = document.getElementById("question");
+        var q2 =question.innerHTML ='What is the minimum amount of arguments "window.localstorage()" will hold?'
+
+        document.getElementById("button1").textContent = "1";
+        document.getElementById("button2").textContent = "2";
+        document.getElementById("button3").textContent = "3";
+        document.getElementById("button4").textContent = "It doesn't need any ";
+
+        document.getElementById("button1").addEventListener("click", function(){
+            wrong()
+         }); 
+ 
+ 
+         document.getElementById("button2").addEventListener("click", function(){
+             question5()
+         });
+ 
+         document.getElementById("button3").addEventListener("click", function(){
+             wrong()
+         });
+         
+         document.getElementById("button4").addEventListener("click", function(){
+             wrong()
+         });
+
+    }
+
+   //question3
+
+   function question3(){
+
+       clearbuttons()
+
+       var question = document.getElementById("question");
+        var q2 =question.innerHTML ='"For" and "While" are considered?'
+
+        document.getElementById("button1").textContent = "Arrays";
+        document.getElementById("button2").textContent = "Functions";
+        document.getElementById("button3").textContent = "Reassignments";
+        document.getElementById("button4").textContent = "Loops";
+
+        document.getElementById("button1").addEventListener("click", function(){
+            wrong()
+         }); 
+ 
+ 
+         document.getElementById("button2").addEventListener("click", function(){
+             wrong()
+         });
+ 
+         document.getElementById("button3").addEventListener("click", function(){
+             wrong()
+         });
+         
+         document.getElementById("button4").addEventListener("click", function(){
+             question4()
+         });
+
+
+     }
+
+
+
+    //question2
+    function question2(){
+
+        clearbuttons()
+    
+        var question = document.getElementById("question")
+        var q2 =question.innerHTML ='What does the Push() Function do?';
+    
+        document.getElementById("button1").textContent = "Pushes content into an array";
+        document.getElementById("button2").textContent = "It removes data from an array";
+        document.getElementById("button3").textContent = "Pushes a string into an integer";
+        document.getElementById("button4").textContent = "removes [i] in for(i=0; i<25);";
+
+        document.getElementById("button1").addEventListener("click", function(){
+           question3()
+        }); 
+
+
+        document.getElementById("button2").addEventListener("click", function(){
+            wrong()
+        });
+
+        document.getElementById("button3").addEventListener("click", function(){
+            wrong()
+        });
+        
+        document.getElementById("button4").addEventListener("click", function(){
+            wrong()
+        });
+
+
+
+    }
+
+
+
+    //this function clones the buttons. As a side effect, it removes the listeners. Since remove event listener wasn't working....
+    function clearbuttons(){
+
+    var button1old = document.getElementById("button1");
+    var button1new = document.getElementById("button1").cloneNode(true);
+    button1old.parentNode.replaceChild(button1new, button1old);
+
+    var button2old = document.getElementById("button2");
+    var button2new = document.getElementById("button2").cloneNode(true);
+    button2old.parentNode.replaceChild(button2new, button2old);
+
+    var button3old = document.getElementById("button3");
+    var button3new = document.getElementById("button3").cloneNode(true);
+    button3old.parentNode.replaceChild(button3new, button3old);
+
+    var button4old = document.getElementById("button4");
+    var button4new = document.getElementById("button4").cloneNode(true);
+    button4old.parentNode.replaceChild(button4new, button4old);
+
+    }
+
     //question1
     function question1(){
+
+        function wrong(){
+            clearInterval(startCountdown);
+            window.alert("Wrong! -10secs, try again");
+    
+            time= time-9
+    
+            timer()
+    
+            startCountdown=setInterval(timer,1000);
+        }
 
         var question = document.getElementById("question")
 
@@ -136,11 +372,13 @@ document.getElementById("startbutton").addEventListener("click",function test(){
         document.getElementById("button4").addEventListener("click", function(){
             wrong()
         });
+            
+        
 
 
     }
 
-question1()
+    question1()
 
 });
  
